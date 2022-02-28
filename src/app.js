372688ea4153a55682f535ce19d92904c1750219
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-02-28 23:45:14 +0200

    work on api documentation

--- src/app.js
+++ src/app.js
@@ -1,55 +1,64 @@
-/* eslint-disable no-console */
-import express from 'express';
-import routes from './routes/index';
-import db from './models';
-import 'dotenv/config';
+import express from "express";
+import routes from "./routes/index.js";
+import db from "./models";
+import cors from "cors";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "../swagger.json";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3000;
 const mode = process.env.NODE_ENV || "development";
-const host = process.env.HOST || "localhost";
 
 try {
-	const { sequelize } = db;
-	//TESTING DATABASE CONNECTION
-	if (mode === "development") {
-		sequelize
-			.authenticate()
-			.then(() => {
-				console.log("DEV DB CONNECTED...");
-			})
-			.catch((err) => {
-				console.log("Unable to connect to the database: ", err);
-			});
-	}
-	if (mode === "test") {
-		sequelize
-			.authenticate()
-			.then(() => {
-				console.log("TEST DB CONNECTED...");
-			})
-			.catch((err) => {
-				console.log("Unable to connect to the database: ", err);
-			});
-	}
-	if (mode === "production") {
-		sequelize
-			.authenticate()
-			.then(() => {
-				console.log("PRODUCTION DB CONNECTED...");
-			})
-			.catch((err) => {
-				console.log("Unable to connect to the database: ", err);
-			});
-	}
-	app.get("/", (req, res) => {
-		res.json({ message: "WELCOME TO THE API" });
-	});
-	app.use("/api/v1", routes);
-	app.listen(port, host, () => {
-		console.log(`The server is running on port ${port}`);
-	});
+  const { sequelize } = db;
+  //TESTING DATABASE CONNECTION
+  if (mode === "development") {
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("DEV DB CONNECTED...");
+      })
+      .catch((err) => {
+        console.log("Unable to connect to the database: ", err);
+      });
+  }
+  if (mode === "test") {
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("TEST DB CONNECTED...");
+      })
+      .catch((err) => {
+        console.log("Unable to connect to the database: ", err);
+      });
+  }
+  if (mode === "production") {
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("PRODUCTION DB CONNECTED...");
+      })
+      .catch((err) => {
+        console.log("Unable to connect to the database: ", err);
+      });
+  }
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use("/api/v1/", routes);
+  app.use(
+    "/docs/swagger-ui/",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDoc, { explorer: true })
+  );
+
+  app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+  });
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 export default app;

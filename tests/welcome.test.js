591c36d7a0a7b3f739e7c1856d473b88ec36e732
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-03-02 17:18:41 +0200

    some minor updates on code

--- tests/welcome.test.js
+++ tests/welcome.test.js
@@ -1,16 +1,28 @@
-import { expect, request, use } from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../src/app';
+import { expect, request, use } from "chai";
+import chaiHttp from "chai-http";
+import app from "../src/app";
 
 use(chaiHttp);
 
-describe('WELCOME TEST', function() {
-    it('SHOULD RETURN 200', function(done){
-        request(app)
-            .get('/api/v1/')
-            .end((err, res)=>{
-                expect(res.statusCode).to.equal(200);
-                done();
-        });
-    });
+describe("WELCOME END-POINT-TEST", () => {
+  it("should display welcome ", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/")
+      .send()
+      .end((err, res) => {
+        chai.expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it("should not display welcome", () => {
+    chai
+      .request(app)
+      .get("/api/v1/welcomeagain")
+      .send()
+      .end((err, res) => {
+        chai.expect(res).to.have.status(404);
+      });
+  });
 });

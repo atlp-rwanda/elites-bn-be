e68486d04c3edc132ebfc5b0c8605bb7ad4d88a6
Honore IRADUKUNDA <ihonore01@gmail.com>
2022-03-03 17:01:00 +0200

    added running environments and modified db connections configuration

--- src/app.js
+++ src/app.js
@@ -1,24 +1,50 @@
 /* eslint-disable no-console */
 import express from 'express';
-import cors from 'cors';
 import routes from './routes/index';
 import db from './models';
 import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mode = process.env.NODE_ENV || "development";
 
 try {
-    app.use(cors());
-    app.use(express.json());
-    app.use('/api/v1/', routes);
-
-    const { sequelize } = db;
-    sequelize.authenticate().then(() => console.log('Database connected...'));
-
-    app.listen(port, () => {
-        console.log(`The server is running on port ${port}`);
-    });
+	const { sequelize } = db;
+	//TESTING DATABASE CONNECTION
+	if (mode === "development") {
+		sequelize
+			.authenticate()
+			.then(() => {
+				console.log("DEV DB CONNECTED...");
+			})
+			.catch((err) => {
+				console.log("Unable to connect to the database: ", err);
+			});
+	}
+	if (mode === "test") {
+		sequelize
+			.authenticate()
+			.then(() => {
+				console.log("TEST DB CONNECTED...");
+			})
+			.catch((err) => {
+				console.log("Unable to connect to the database: ", err);
+			});
+	}
+	if (mode === "production") {
+		sequelize
+			.authenticate()
+			.then(() => {
+				console.log("PRODUCTION DB CONNECTED...");
+			})
+			.catch((err) => {
+				console.log("Unable to connect to the database: ", err);
+			});
+	}
+	app.use("/api/v1", routes);
+	app.listen(port, () => {
+		console.log(`The server is running on port ${port}`);
+	});
 } catch (error) {
     console.log(error);
 }

1c6f2d9feb517d1df1ed90a0dabb679a0e34ed63
gihozoinnocente <igihozoinnocente@gmail.com>
2022-03-03 10:48:09 +0200

    Login a user

---
+++ src/models/users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import bcrypt from 'bcrypt';
+
+export default (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        message: 'Please enter your username'
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: {
+        args: false,
+        message: 'Please enter your email'
+      }
+    },
+    image: DataTypes.STRING,
+    hash: DataTypes.STRING
+  }, {
+    hooks: {
+      beforeCreate: async (user) => {
+        user.hash = await bcrypt.hashSync(user.hash, 8);
+      },
+    },
+    instanceMethods: {
+      validatePassword: async function(hash) {
+        return await bcrypt.compareSync(hash, this.password);
+      }
+    }
+  });
+  Users.associate = () => {
+  };
+  return Users;
+};

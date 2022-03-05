7df93d27b5f4d432491c73f44b249fc717129200
niyongaboaristide17 <niyongaboaristide17@gmail.com>
2022-03-05 07:43:05 +0200

    test: update welcome.test.js

--- tests/welcome.test.js
+++ tests/welcome.test.js
@@ -4,12 +4,13 @@ import app from '../src/app'
 
 use(chaiHttp)
 
-describe('WELCOME TEST', () => {
-    it('SHOULD RETURN 200', () => {
+describe('WELCOME TEST', function() {
+    it('SHOULD RETURN 200', function(done){
         request(app)
         .get('/api/v1/')
         .end((err, res)=>{
             expect(res.statusCode).to.equal(200)
+            done()
         })
     });
 })

2a3b6c686db27f31c96e2d1cce9a5d5b11dfdb64
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2022-03-08 14:52:05 +0200

    all good here

--- src/app.js
+++ src/app.js
@@ -1,61 +1,61 @@
-import express from "express";
-import routes from "./routes/index.js";
-import db from "./models";
-import cors from "cors";
-import morgan from "morgan";
-import swaggerUi from "swagger-ui-express";
-import swaggerDoc from "../swagger.json";
-import "dotenv/config";
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+import morgan from 'morgan';
+import routes from './routes/index.js';
+import db from './models';
+import swaggerDoc from '../swagger.json';
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mode = process.env.NODE_ENV || "development";
+const mode = process.env.NODE_ENV || 'development';
 
 try {
   const { sequelize } = db;
   //TESTING DATABASE CONNECTION
-  if (mode === "development") {
+  if (mode === 'development') {
     sequelize
       .authenticate()
       .then(() => {
-        console.log("DEV DB CONNECTED...");
+        console.log('DEV DB CONNECTED...');
       })
       .catch((err) => {
-        console.log("Unable to connect to the database: ", err);
+        console.log('Unable to connect to the database: ', err);
       });
   }
-  if (mode === "test") {
+  if (mode === 'test') {
     sequelize
       .authenticate()
       .then(() => {
-        console.log("TEST DB CONNECTED...");
+        console.log('TEST DB CONNECTED...');
       })
       .catch((err) => {
-        console.log("Unable to connect to the database: ", err);
+        console.log('Unable to connect to the database: ', err);
       });
   }
-  if (mode === "production") {
+  if (mode === 'production') {
     sequelize
       .authenticate()
       .then(() => {
-        console.log("PRODUCTION DB CONNECTED...");
+        console.log('PRODUCTION DB CONNECTED...');
       })
       .catch((err) => {
-        console.log("Unable to connect to the database: ", err);
+        console.log('Unable to connect to the database: ', err);
       });
   }
 
   app.use(cors());
   app.use(express.json());
-  app.use(morgan("dev"));
-  app.use("/api/v1/", routes);
+  app.use(morgan('dev'));
+  app.use('/api/v1/', routes);
 
   app.use(
-    "/docs/swagger-ui/",
+    '/docs/swagger-ui/',
     swaggerUi.serve,
     swaggerUi.setup(swaggerDoc, {
       swaggerOptions: {
-        docExpansions: "none",
+        docExpansions: 'none',
         persistAuthorization: true,
       },
     })

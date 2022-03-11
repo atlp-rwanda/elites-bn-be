2f8fbdf6d1e39fb2d22f964fe78c5325f5b28363
PatrickTUy <yangeney@gmail.com>
2022-03-12 00:02:36 +0200

    added changes on update role functionality

--- src/validation/roleValidation.js
+++ src/validation/roleValidation.js
@@ -3,18 +3,16 @@ import Joi from "joi";
 export const roleValidate = (req, res, next) => {
   const roleValidation = Joi.object({
     email: Joi.string().required().email(),
-    role: Joi.string()
-      .required()
-      .valid("requester","accommodation-supplier", "manager", "travel-admin", "admin")
-      .trim(),
+    
+   
   });
-  const result = roleValidation.validate(req.body);
+  const result = roleValidation.validate(req.body,req.params.id);
  
 
   if (result.error) {
-    return res.status(400).json({ message:result.error.details[0].message });
+    return res.status(400).json({ message:result.error.details[0].message.replace(/["'`]+/g, "") });
   
-  }
+  } 
   next();
 };
 

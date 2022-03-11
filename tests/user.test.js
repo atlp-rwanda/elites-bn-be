948d8089a159d31d69b258c92c7cb6a685075a45
PatrickTUy <yangeney@gmail.com>
2022-03-12 00:02:36 +0200

    added changes on update role functionality

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import { expect, request, use } from 'chai';
 // import chaiHttp from 'chai-http';
 // import app from '../src/app.js'
@@ -23,3 +24,41 @@
     
 //   });
 // });
+=======
+import { expect, request, use } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/app.js'
+
+
+use(chaiHttp);
+
+describe('USER END-POINT TESTING', () => {
+  describe('REGISTER USER TESTS', () => {
+    it('should register a user', async () => {
+      const res = await request(app).post('/api/v1/users/register')
+        .send({
+            names:'Diallo',
+          email: 'diallopeg@gmail.com',
+          password:'diallo'
+        });
+    
+        expect(res).to.have.status([201]);
+      
+     
+    });
+    it('should not register a user when already existing', async () => {
+        const res = await request(app).post('/api/v1/users/register')
+        
+          .send({
+              names:'seeduser',
+            email: 'seeduser@gmail.com',
+            password:'seeduser'
+          });
+          expect(res).to.have.status([409]);
+          
+       
+      });
+    
+  });
+});
+>>>>>>> 72b4048 (added changes on update role functionality)

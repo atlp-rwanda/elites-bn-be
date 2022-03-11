2c3c167d2880784b03d75a17cdd635ea851333a5
PatrickTUy <yangeney@gmail.com>
2022-03-11 14:57:40 +0200

    added update role

--- src/constants/user-constants.js
+++ src/constants/user-constants.js
@@ -2,3 +2,7 @@ export const USER_EXIST = 'User with this email exist.';
 export const USER_REGISTERED = 'User registered successfully please check the email to verify the account.';
 export const USER_LOGIN = 'User logged in successfully.';
 export const INVALID_LOGIN = 'Invalid credentials';
+
+export const USER_NOT_EXIST = 'User with this email does not exist exist.';
+
+

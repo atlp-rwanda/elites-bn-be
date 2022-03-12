e50bc72b0d76ef2e85988993693522788f3cff90
PatrickTUy <yangeney@gmail.com>
2022-03-12 19:41:53 +0200

    minor changes

--- tests/role.test.js
+++ tests/role.test.js
@@ -8,18 +8,18 @@ use(chaiHttp);
 describe('ROLE END-POINT TESTING', () => {
   describe('UPDATE ROLE USER TESTS', () => {
     it('should assign a role to a user', async () => {
-        // let id = 4;
+    
       const res = await request(app).patch('/api/v1/users/updateRole/4')
         .send({
           email: 'seeduser@gmail.com',
         });
         expect(res).to.have.status([200]);
-        // expect(res.body).to.have.roleId(4)
+      
      
     });
 
     it("should not assign a role to a user if role doesn't exist", async () => {
-        let id = 10;
+    
       const res = await request(app)
         .patch('/api/v1/users/updateRole/9')
         .send({

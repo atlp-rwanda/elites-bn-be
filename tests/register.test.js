df38c388bfb2cdb4faaaf71952d5fa5531fdebea
gihozoinnocente <igihozoinnocente@gmail.com>
2022-03-16 08:02:12 +0200

    Fixed registration test

--- tests/register.test.js
+++ tests/register.test.js
@@ -1,15 +1,17 @@
 import { expect, request, use } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../src/app.js';
-import User from "./../src/models/user.js"
-
+import models from '../src/models';
 use(chaiHttp);
 
 describe('USER REGISTER A USER', () => {
- 
+
+  before(async ()=>{
+    await models.User.destroy({where:{email:'elites@gmail.com'}})
+ })
     it('it should register the user', async () => {
       const res = await request(app).post('/api/v1/users/register').send({
-        email: 'giho24@gmail.com',
+        email: 'elites@gmail.com',
         password: 'Pass12515858'
       })
       expect(res).to.have.status([200]);

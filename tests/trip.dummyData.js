eee16f60a87df879f202be725fbcb43e0e7cfa7a
Dev-Nkundineza <niyonzimadeus2002@gmail.com>
2022-03-16 13:05:51 +0200

    some files changes

--- tests/trip.dummyData.js
+++ tests/trip.dummyData.js
@@ -1,35 +1,48 @@
 export const addRequest = {
-    managerId: 3,
     departLocation: 2,
     arrivalLocation: 5,
     tripReason: "travelling and studies",
     departDate: "2022-05-01",
     returnDate: "2022-06-04",
-    travelId: 1,
-    accomodationId: 6,
+    accomodationId: 6
 
 }
 
 export const incorrectDate = {
-    managerId: 3,
     departLocation: 2,
     arrivalLocation: 5,
     tripReason: "travelling for research and studies",
     departDate: "2022-05-01",
     returnDate: "2022-04-04",
-    travelId: 1,
-    accomodationId: 6,
+    accomodationId: 6
 
 }
 
 export const updateRequest = {
-    managerId: 3,
     departLocation: 2,
     arrivalLocation: 5,
     tripReason: "travelling and studies updated data",
     departDate: "2022-05-01",
     returnDate: "2022-06-04",
-    travelId: 1,
     accomodationId: 6,
 
+}
+
+
+export const managerLogin = {
+    email: "jane@gmail.com",
+    password: "111@call"
+}
+
+// with no manager 
+export const requesterLogin = {
+    email: "senderthree@gmail.com",
+    password: "pass123@"
+}
+
+// with manager
+
+export const requesterLoginWithManager = {
+    email: "sendertwo@gmail.com",
+    password: "pass123@"
 }

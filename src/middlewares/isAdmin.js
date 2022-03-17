3a12e6d2b3c07a22b99d0f30b6f1397ab39eb773
PatrickTUy <yangeney@gmail.com>
2022-03-17 13:39:25 +0200

    updated tests

---
+++ src/middlewares/isAdmin.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import { decodeAcessToken } from '../helpers/jwtFunction';
+import models from '../models'
+
+export const isAdmin = async (req, res, next) => {
+  
+    try{
+      let emptyToken = req.headers.authorization
+    if(emptyToken === null){
+     return res.status(403).json({ message: 'user is not logged in' });
+      }
+const token = req.headers.authorization.split(' ')[1];
+    let data;
+
+      data = await decodeAcessToken(token);
+      console.log(data)
+
+      const user = await models.User.findOne({where:{id:data.id},
+        include:'Role'})
+        console.log(user.roleId)
+        if (user.roleId !== 1)
+        return res.status(401).json({
+          status: 401,
+          message: 'Only admins are allowed to perform this task',
+        });
+    }catch (error) {
+      return res.status(403).json({ message: 'user is not logged in' });
+    } 
+
+    
+
+    next();
+  
+};
--- src/seeders/20220309202416-User.js
+++ src/seeders/20220309202416-User.js
@@ -1,11 +1,31 @@
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10, 'b');
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
-    'Users',
-    [
-      {
-        names: 'Gihozo Innocente',
-        email: 'gihozo97@gmail.com',
-        password:
+	up: (queryInterface, Sequelize) =>
+		queryInterface.bulkInsert(
+			'Users',
+			[
+
+				{
+					names:'YANGENEYE Patrick',
+					email:'yangeney@gmail.com',
+					password:await bcrypt.hash('password', salt),
+					roleId:1,
+					createdAt: new Date(),
+			     	updatedAt: new Date()
+				},
+				{
+					names:'seeduser',
+  					email:'seeduser@gmail.com',
+    				password:await bcrypt.hash('seeduser', salt),
+					roleId:5,
+					createdAt: new Date(),
+					updatedAt: new Date()
+				},
+				{
+					names: 'Gihozo Innocente',
+					email: 'gihozo97@gmail.com',
+					password:
 						'$2b$10$UXnwEWSIsSXsK/ocfmr0R.7OAt3WD1WpD0kx5VtI/wNbvQO1iGxWa',
         managerId: 1,
         createdAt: new Date(),

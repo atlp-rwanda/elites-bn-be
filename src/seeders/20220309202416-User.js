3a9c1b347db624388895473a64afe5737a55b056
PatrickTUy <yangeney@gmail.com>
2022-03-17 22:59:21 +0200

    minorchanges

--- src/seeders/20220309202416-User.js
+++ src/seeders/20220309202416-User.js
@@ -1,40 +1,29 @@
+'use strict';
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(10, 'b');
-module.exports = {
-	up: (queryInterface, Sequelize) =>
-		queryInterface.bulkInsert(
-			'Users',
-			[
 
-				{
-					names:'YANGENEYE Patrick',
-					email:'yangeney@gmail.com',
-					password:await bcrypt.hash('password', salt),
-					roleId:1,
-					createdAt: new Date(),
-			     	updatedAt: new Date()
-				},
-				{
-					names:'seeduser',
-  					email:'seeduser@gmail.com',
-    				password:await bcrypt.hash('seeduser', salt),
-					roleId:5,
-					createdAt: new Date(),
-					updatedAt: new Date()
-				},
-				{
-					names: 'Gihozo Innocente',
-					email: 'gihozo97@gmail.com',
-					password:
-						'$2b$10$UXnwEWSIsSXsK/ocfmr0R.7OAt3WD1WpD0kx5VtI/wNbvQO1iGxWa',
-        managerId: 1,
+module.exports = {
+  async up (queryInterface, Sequelize) {
+      await queryInterface.bulkInsert('Users', [{
+      names:'YANGENEYE Patrick',
+      email:'yangeney@gmail.com',
+      password:await bcrypt.hash('password', salt),
+        roleId:1,
         createdAt: new Date(),
-        updatedAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        names: 'Honore Iradukunda',
-        email: 'ihonore03@gmail.com',
-        password:
+        names:'seeduser',
+        email:'seeduser@gmail.com',
+        password:await bcrypt.hash('seeduser', salt),
+          roleId:5,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+					names: 'Honore Iradukunda',
+					email: 'ihonore03@gmail.com',
+					password:
 						'$2b$10$QYWjjX0DW7Qhgqf30Gxh3uZ1IkPy2pGYa14069VwbnoRsdlzQE1om',
         roleId: 3,
         managerId: 1,
@@ -56,15 +45,16 @@ module.exports = {
         email: 'ihonore@gmail.com',
         password:
 						'$2b$10$ae70fNT92f9eK.ZhcgNlzOQQb36Ba43Mh37UNxGi9Ey8bcOsONDoi',
-        roleId: 5,
-        managerId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ],
+					roleId: 5,
+					managerId: 1,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},], {});
+    
+  },
 
-    {},
-  ),
-
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null, {}),
+  async down (queryInterface, Sequelize) {
+      await queryInterface.bulkDelete('Users', null, {});
+     
+  }
 };

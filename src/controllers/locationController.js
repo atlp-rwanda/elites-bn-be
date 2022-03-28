90c460f784bd8772740c364451ffad6437438eba
Niyongabo Aristide <niyongaboaristide17@gmail.com>
2022-03-28 16:43:12 +0200

    fix some issue after rebase

--- src/controllers/locationController.js
+++ src/controllers/locationController.js
@@ -3,77 +3,77 @@ const locationService = new locationServices();
 import { BaseError } from '../httpErrors/baseError';
 
 class LocationControllers {
-	createLocation = async (req, res, next) => {
-		try {
-			const data = {
-				locationName: req.body.locationName,
-				description: req.body.description,
-				country: req.body.country,
-			};
-			const location = await locationService.createLocation(data);
-			return res.status(201).json({
-				status: '201',
-				message: 'location added successfully',
-				payload: location,
-			});
-		} catch (err) {
-			next(err);
-		}
-	};
+    createLocation = async(req, res, next) => {
+        try {
+            const data = {
+                locationName: req.body.locationName,
+                description: req.body.description,
+                country: req.body.country,
+            };
+            const location = await locationService.createLocation(data);
+            return res.status(201).json({
+                status: '201',
+                message: 'location added successfully',
+                payload: location,
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
 
-	getSingleLocation = async (req, res, next) => {
-		try {
-			const foundLocation = await locationService.getSingleLocation(
-				req.params.locationId
-			);
-			if (foundLocation) {
-				return res.status(200).json({
-					status: '200',
-					message: 'Location found',
-					payload: foundLocation,
-				});
-			} else
-				throw new BaseError(
-					'Not found',
-					404,
-					'Location with that ID does not exists'
-				);
-		} catch (err) {
-			next(err);
-		}
-	};
+    getSingleLocation = async(req, res, next) => {
+        try {
+            const foundLocation = await locationService.getSingleLocation(
+                req.params.locationId
+            );
+            if (foundLocation) {
+                return res.status(200).json({
+                    status: '200',
+                    message: 'Location found',
+                    payload: foundLocation,
+                });
+            } else
+                throw new BaseError(
+                    'Not found',
+                    404,
+                    'Location with that ID does not exists'
+                );
+        } catch (err) {
+            next(err);
+        }
+    };
 
-	updateLocation = async (req, res, next) => {
-		try {
-			const locationUpdate = {
-				locationName: req.body.locationName,
-				description: req.body.description,
-				country: req.body.country,
-			};
-			const location = await locationService.updateLocation(
-				req.params.locationId,
-				locationUpdate
-			);
-			res.status(200).json({
-				status: 200,
-				message: 'location updated successfully',
-				payload: location,
-			});
-		} catch (err) {
-			next(err);
-		}
-	};
+    updateLocation = async(req, res, next) => {
+        try {
+            const locationUpdate = {
+                locationName: req.body.locationName,
+                description: req.body.description,
+                country: req.body.country,
+            };
+            const location = await locationService.updateLocation(
+                req.params.locationId,
+                locationUpdate
+            );
+            res.status(200).json({
+                status: 200,
+                message: 'location updated successfully',
+                payload: location,
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
 
-	deleteLocation = async (req, res, next) => {
-		try {
-			const deleteMessage = await locationService.deleteLocation(
-				req.params.locationId
-			);
-			res.status(200).json({ status: 200, message: deleteMessage });
-		} catch (err) {
-			next(err);
-		}
-	};
+    deleteLocation = async(req, res, next) => {
+        try {
+            const deleteMessage = await locationService.deleteLocation(
+                req.params.locationId
+            );
+            res.status(200).json({ status: 200, message: deleteMessage });
+        } catch (err) {
+            next(err);
+        }
+    };
 }
 
 export default LocationControllers;
--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -2,95 +2,95 @@ import roomServices from '../services/roomServices';
 import { BaseError } from '../httpErrors/baseError';
 const roomService = new roomServices();
 class RoomControllers {
-	createRoom = async (req, res, next) => {
-		try {
-			const data = {
-				roomType: req.body.roomType,
-				roomNumber: req.body.roomNumber,
-				price: req.body.price,
-				currency: req.body.currency,
-				isAvailable: req.body.isAvailable,
-				accommodationId: req.body.accommodationId,
-			};
-			const room = await roomService.createRoom(data);
-			return res.status(201).json({
-				status: '201',
-				message: 'room added successfully',
-				payload: room,
-			});
-		} catch (err) {
-			next(err);
-		}
-	};
+    createRoom = async(req, res, next) => {
+        try {
+            const data = {
+                roomType: req.body.roomType,
+                roomNumber: req.body.roomNumber,
+                price: req.body.price,
+                currency: req.body.currency,
+                isAvailable: req.body.isAvailable,
+                accommodationId: req.body.accommodationId,
+            };
+            const room = await roomService.createRoom(data);
+            return res.status(201).json({
+                status: '201',
+                message: 'room added successfully',
+                payload: room,
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
 
-	getAllRoomsOfAccommodation = async (req, res, next) => {
-		try {
-			const foundRooms = await roomService.getAllRoomsOfAccommodation(
-				req.params.accommodationId
-			);
-			if (foundRooms.length !== 0) {
-				return res.status(200).json({
-					status: '200',
-					message: 'All rooms in given accommodation',
-					payload: foundRooms,
-				});
-			} else
-				throw new BaseError(
-					'Not found',
-					404,
-					'No rooms found in given accommodation'
-				);
-		} catch (err) {
-			next(err);
-		}
-	};
+    getAllRoomsOfAccommodation = async(req, res, next) => {
+        try {
+            const foundRooms = await roomService.getAllRoomsOfAccommodation(
+                req.params.accommodationId
+            );
+            if (foundRooms.length !== 0) {
+                return res.status(200).json({
+                    status: '200',
+                    message: 'All rooms in given accommodation',
+                    payload: foundRooms,
+                });
+            } else
+                throw new BaseError(
+                    'Not found',
+                    404,
+                    'No rooms found in given accommodation'
+                );
+        } catch (err) {
+            next(err);
+        }
+    };
 
-	getSingleRoom = async (req, res, next) => {
-		try {
-			const foundRoom = await roomService.getSingleRoom(req.params.roomId);
-			if (!foundRoom)
-				throw new BaseError(
-					'Not found',
-					404,
-					'Room with that ID does not exists'
-				);
-			return res
-				.status(200)
-				.json({ status: '200', message: 'Room found', payload: foundRoom });
-		} catch (err) {
-			next(err);
-		}
-	};
+    getSingleRoom = async(req, res, next) => {
+        try {
+            const foundRoom = await roomService.getSingleRoom(req.params.roomId);
+            if (!foundRoom)
+                throw new BaseError(
+                    'Not found',
+                    404,
+                    'Room with that ID does not exists'
+                );
+            return res
+                .status(200)
+                .json({ status: '200', message: 'Room found', payload: foundRoom });
+        } catch (err) {
+            next(err);
+        }
+    };
 
-	updateRoom = async (req, res, next) => {
-		try {
-			const roomUpdate = {
-				roomType: req.body.roomType,
-				roomNumber: req.body.roomNumber,
-				price: req.body.price,
-				isAvailable: req.body.isAvailable,
-				currency: req.body.currency,
-				accommodationId: req.body.accommodationId,
-			};
-			const room = await roomService.updateRoom(req.params.roomId, roomUpdate);
-			res.status(200).json({
-				status: 200,
-				message: 'room updated successfully',
-				payload: room,
-			});
-		} catch (err) {
-			next(err);
-		}
-	};
+    updateRoom = async(req, res, next) => {
+        try {
+            const roomUpdate = {
+                roomType: req.body.roomType,
+                roomNumber: req.body.roomNumber,
+                price: req.body.price,
+                isAvailable: req.body.isAvailable,
+                currency: req.body.currency,
+                accommodationId: req.body.accommodationId,
+            };
+            const room = await roomService.updateRoom(req.params.roomId, roomUpdate);
+            res.status(200).json({
+                status: 200,
+                message: 'room updated successfully',
+                payload: room,
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
 
-	deleteRoom = async (req, res, next) => {
-		try {
-			const deleteMessage = await roomService.deleteRoom(req.params.roomId);
-			res.status(200).json({ status: 200, message: deleteMessage });
-		} catch (err) {
-			next(err);
-		}
-	};
+    deleteRoom = async(req, res, next) => {
+        try {
+            const deleteMessage = await roomService.deleteRoom(req.params.roomId);
+            res.status(200).json({ status: 200, message: deleteMessage });
+        } catch (err) {
+            next(err);
+        }
+    };
 }
 
 export default RoomControllers;

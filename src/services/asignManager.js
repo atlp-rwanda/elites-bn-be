d668cea88cf715eb5aa78a5c3fed2ffc17b5c95d
Dev-Nkundineza <niyonzimadeus2002@gmail.com>
2022-06-10 10:39:48 +0200

    changes on the assign manager

--- src/services/asignManager.js
+++ src/services/asignManager.js
@@ -3,6 +3,6 @@ import models from '../models';
 export const assignManager = async (userId, managerId) => {
   const user = await models.User.findOne({ where: { id: userId } });
     user.managerId = managerId;
-    user.save();
+    await user.save();
     return user;
 };
